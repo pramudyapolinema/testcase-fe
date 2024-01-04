d90e8837259c46d9217b834da692fbb2135e71ce
pramudyawibowo <pramudya.wibowo72@gmail.com>
2024-01-04 12:32:36 +0700

    fix cors

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var logger = require("morgan");
 var bodyParser = require("body-parser");
 var multer = require("multer");
 var forms = multer();
+var cors = require("cors");
 
 var indexRouter = require("./routes/index");
 var productsRouter = require("./routes/products");
@@ -14,6 +15,7 @@ app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(forms.array());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
 
 app.use("/", indexRouter);
 app.use("/api/v1/data", productsRouter);
